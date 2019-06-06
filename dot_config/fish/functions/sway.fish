e82767f3b561cf52df5305da54dff77fff31b56f
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-06-06 15:53:17 +0300

    Added web wallpaper server
    For use as homepage and with New Tab Homepage WebExtension

--- dot_config/fish/functions/sway.fish
+++ dot_config/fish/functions/sway.fish
@@ -1,7 +1,14 @@
 function sway
-	set -x QT_QPA_PLATFORM wayland-egl
+set -x QT_QPA_PLATFORM wayland-egl
 set -x CLUTTER_BACKEND wayland
 set -x SDL_VIDEODRIVER wayland
 set -x GDK_BACKEND wayland
+set -x WALLPAPERS_DIR ~/Attēli/wallpapers
+
+systemctl --user set-environment WALLPAPERS_DIR="$WALLPAPERS_DIR"
+systemctl --user start html-wallpaper@8000.service
+
 /usr/bin/sway
+
+systemctl --user stop html-wallpaper@8000.service
 end
