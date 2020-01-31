009e6ec1e57d115944c90b7d5e772c7fc746808b
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2020-01-31 22:36:16 +0200

    Added autocompletion for tmux-session function

--- dot_config/fish/functions/tmux-session.fish
+++ dot_config/fish/functions/tmux-session.fish
@@ -1,6 +1,6 @@
 function tmux-session
   set session "$HOME/.config/tmux-sessions/$argv"
-  if tmux ls | grep -w -e "$argv" -q
+  if tmux has-session -t "$argv"
     tmux attach -t "$argv"
   else
     if test -f "$session"
@@ -10,3 +10,17 @@ function tmux-session
     end
   end
 end
+
+function __list_all_tmux_sessions
+  set session_dir "$HOME/.config/tmux-sessions/"
+  if test -d $session_dir
+    find -L "$session_dir" -mindepth 1 -maxdepth 1 -type f -printf '%f\n'
+  end
+  tmux ls -F '#S Running session' 2>>/dev/null
+end 
+
+function __complete_tmux_session
+__list_all_tmux_sessions | awk '{ sess[$1] = $0 ;} END { for ( i in sess ) print sess[i]; }'
+end
+
+complete -c tmux-session -a '(__complete_tmux_session | sed \'s/ /\\t/1\')' -f
